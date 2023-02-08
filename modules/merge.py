e497f2b3a891b21cfcb699605ddfde9a667aa922
Kamyar Roshan <58539929+kamyar-roshan@users.noreply.github.com>
2023-02-08 16:13:12 -0500

    Update and rename merge.py to modules/merge.py

--- modules/merge.py
+++ modules/merge.py
@@ -1,7 +1,6 @@
 from modules.search import search
 
 def merge(values, key1, key2):
-    
     v = str(search(values, key2))
     for i in values[str(search(values, key2))]:
         values[str(search(values, key1))].append(i)
